7ba19fc1bbee34e244df9a112ccd47c130ab32dc
Paulo Linhares <paulitolinhares@gmail.com>
2016-05-05 11:46:17 -0300

    Add email validation on database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  # Before Save
+  before_save { self.email = email.downcase }
+  #Validations
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
